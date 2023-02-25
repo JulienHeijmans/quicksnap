14d65b0b4ccf02309c9a0b4f52054f714f831792
Julien Heijmans <julienh@unity3d.com>
2023-02-25 15:18:31 +0100

    Cleanup

--- quicksnap_render.py
+++ quicksnap_render.py
@@ -27,45 +27,48 @@ if bpy.app.version >= (3, 4, 0):
     shader_3d_polyline_smooth_color = gpu.shader.from_builtin('POLYLINE_SMOOTH_COLOR')
 
 shader_2d_image_info = gpu.types.GPUShaderCreateInfo()
-#interface
-shader_2d_image_interface = gpu.types.GPUStageInterfaceInfo("shader_2d_image_interface")
-shader_2d_image_interface.smooth('VEC2', "uvInterp")
-# vertex shader info/interface
+
+# Vertex shader inputs and uniforms that are now called constants.
 shader_2d_image_info.vertex_in(0, 'VEC2', "texCoord")
 shader_2d_image_info.vertex_in(1, 'VEC2', "pos")
+shader_2d_image_info.sampler(0, 'FLOAT_2D', "Image")
 shader_2d_image_info.push_constant('MAT4', "ModelViewProjectionMatrix")
+shader_2d_image_info.push_constant('VEC4', "Color")
+shader_2d_image_info.push_constant('VEC4', "Color_bg")
+shader_2d_image_info.push_constant('FLOAT', "Fade")
 
-# fragment shader info/interface
-shader_2d_image_info.push_constant('VEC4', "u_Color")
-shader_2d_image_info.push_constant('VEC4', "u_Color_bg")
-shader_2d_image_info.push_constant('FLOAT', "u_Fade")
-shader_2d_image_info.sampler(0, 'FLOAT_2D', "u_Image")
-
+# Define as Interface the attributes that will be transferred from the vertex shader to the fragment shader.
+# Before they would be both a vertex shader output and fragment shader input.
+# An interface can be flat(), no_perspective() or smooth()
+shader_2d_image_interface = gpu.types.GPUStageInterfaceInfo("shader_2d_image_interface")
+shader_2d_image_interface.smooth('VEC2', "texCoord_interp")
 shader_2d_image_info.vertex_out(shader_2d_image_interface)
+
+# fragment shader output
+shader_2d_image_info.fragment_out(0, 'VEC4', 'fragColor')
+
 shader_2d_image_info.vertex_source(
     '''
     void main()
     {
       gl_Position = ModelViewProjectionMatrix * vec4(pos.xy, 0.0f, 1.0f);
       gl_Position.z = 1.0;
-      uvInterp = texCoord;
+      texCoord_interp = texCoord;
     }
     ''')
-shader_2d_image_info.fragment_out(0, 'VEC4', 'fragColor')
 shader_2d_image_info.fragment_source(
     '''
     void main()
     {
-      vec4 texture_sampled=texture(u_Image, uvInterp);
+      vec4 texture_sampled=texture(Image, texCoord_interp);
       if(texture_sampled.a>0.6)
-        fragColor = u_Color * texture_sampled;
+        fragColor = Color * texture_sampled;
       else
-        fragColor = u_Color_bg * texture_sampled;
-      fragColor=vec4(fragColor.r, fragColor.g, fragColor.b, fragColor.a * smoothstep(0,1,u_Fade));
+        fragColor = Color_bg * texture_sampled;
+      fragColor=vec4(fragColor.r, fragColor.g, fragColor.b, fragColor.a * smoothstep(0,1,Fade));
     }
 
     ''')
-# shader_2d_image_color = gpu.types.GPUShader(shader_2d_image_vertex, shader_2d_image_fragment)
 shader_2d_image_color = gpu.shader.create_from_info(shader_2d_image_info)
 del shader_2d_image_info
 del shader_2d_image_interface
@@ -137,10 +140,10 @@ def draw_image(self, position_x=100, position_y=100, size=20, image='MISSING', c
         },
     )
     shader_2d_image_color.bind()
-    shader_2d_image_color.uniform_float("u_Color", color)
-    shader_2d_image_color.uniform_float("u_Color_bg", color_bg)
-    shader_2d_image_color.uniform_float("u_Fade", fade)
-    shader_2d_image_color.uniform_sampler("u_Image", icons[image])
+    shader_2d_image_color.uniform_float("Color", color)
+    shader_2d_image_color.uniform_float("Color_bg", color_bg)
+    shader_2d_image_color.uniform_float("Fade", fade)
+    shader_2d_image_color.uniform_sampler("Image", icons[image])
     batch.draw(shader_2d_image_color)
 
     # if image and self.icons[image].gl_load():
