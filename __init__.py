b60521b8745f0a21f54f108a173bdebbb7ba5def
Julien Heijmans <julienh@unity3d.com>
2023-05-29 16:35:30 +0200

    Test 4.0a fix

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "QuickSnap",
     "author": "Julien Heijmans",
     "blender": (2, 93, 0),
-    'version': (1, 4, 4),
+    'version': (1, 4, 5),
     "category": "3D View",
     "description": "Quickly snap objects/vertices/curve points",
     "warning": "",
