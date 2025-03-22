10cbab5b1aef276a2cfbc8834b116412b16eedf1
Julien Heijmans <julienh@unity3d.com>
2025-03-22 11:44:58 +0100

    Revert "1.4.8 Fix blender type subclassing for Blender 4 4"
    This reverts commit 5fd1b949f2af9642ca230ce7493c02f319df23cf.

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "QuickSnap",
     "author": "Julien Heijmans",
     "blender": (2, 93, 0),
-    'version': (1, 4, 8),
+    'version': (1, 4, 7),
     "category": "3D View",
     "description": "Quickly snap objects/vertices/curve points",
     "warning": "",
